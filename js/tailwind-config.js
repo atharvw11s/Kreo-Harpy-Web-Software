008377b031b88dc018843dd5e0d05f03e76bd03d
atharv <atharvgamer510@gmail.com>
2025-10-01 13:08:25 +0530

    Update tailwind-config.js

--- js/tailwind-config.js
+++ js/tailwind-config.js
@@ -1,16 +1,16 @@
 tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                'kreo-dark': '#0d0d0d',
-                'kreo-card': '#1a1a1a',
-                'kreo-accent': '#6600FF',
-                'kreo-connected': '#00ff73',
-                'kreo-off-gray': '#4b5563',
-            },
-            fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-            },
-        },
-    },
-};
+    theme: {
+        extend: {
+            colors: {
+                'kreo-dark': '#0d0d0d',
+                'kreo-card': '#1a1a1a',
+                'kreo-accent': '#6600FF', 
+                'kreo-connected': '#00ff73',
+                'kreo-off-gray': '#4b5563',
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        }
+    }
+}
